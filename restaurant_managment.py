062a1f64768bbb806b35784106ae05a220f2f393
MissLyd <165266642+MissLyd@users.noreply.github.com>
2025-09-02 11:19:44 +0100

    Updated order taking
    Added a rating option to the customer orders as well as a sales report including items, the quantity sold per item and the grand total

--- restaurant_managment.py
+++ restaurant_managment.py
@@ -1,14 +1,20 @@
 
 class Restaurant:
     def __init__(self):
-        self.menu_items = {} # Dict
-        self.book_table = {}
-        self.customer_orders = []
-        self.waitlist = []
+        self.menu_items = {} # menu_items = { item: {"price": price,"Season":season} }
+        self.book_table = {} # book_table = {"table nbr": customer name}
+        self.customer_orders = [] # customer_orders= [{ 
+                                                        #"table": table_nbr,
+                                                        #"customer": customer name, 
+                                                        # "items": [(item_name, qty)],
+                                                        #  "total": float,
+                                                        # "rating": float,
+                                                        # }]
+        self.waitlist = [] # waitlist= [(customer name, table nbr)]
         self.total_revenue = None
     
 
-    # ------------------------Menu------------------------
+    # ------------------------------------------------Menu------------------------------------------------
 
     # 1- Adding a menu item with its positive price and valid season
     def add_item_to_menu(self,item,price = None,season = None,valid_seasons=None):
@@ -111,7 +117,7 @@ class Restaurant:
                 print(f"{item}: {details["price"]}da, Season: {details["season"]}")
             
 
-    # -----------------------Tables----------------------------
+    # -----------------------------------------------Tables----------------------------------------------------
 
     #1- Booking tables or pushing to waitlist if the table is already booked
     def book_tables(self,table_nbr):
@@ -156,24 +162,139 @@ class Restaurant:
         else:
             print("No reservations yet.") # If empty
 
-    # Orders
-    def customer_order(self,order,table_nbr):
-        if order not in self.menu_items:
-            print("This item is not available.")
-        elif table_nbr not in self.book_table:
-            print("This table is not booked.")
-        else:
-            order_details ={"table_number": table_nbr, "order": order}
-            self.customer_orders.append(order_details)
-            print(f"Order taken for table {table_nbr}: {order}")
+    # ------------------------------------------------Orders------------------------------------------------
 
-    def print_orders(self):
-        print("Customer orders:")
-        if self.customer_orders:
-            for order_details in self.customer_orders:
-                print(f"Table {order_details['table_nbr']}: {order_details['order']}")
+    #1- Taking an order
+    def customer_order(self,table_nbr):
+
+        # Checking if the table is availabke
+        if table_nbr not in self.book_table:
+            print("This table is not booked.")
+            return
+        
+        # Input customer name
+        customer_name=input("Enter your name: ").strip()
+        order=[]
+        total=0
+
+        while True:
+            # Asking for items of order one by one 
+            suborder= input("Enter one item of your order or 0 to finish: ").strip
+
+            # Exiting the loop
+            if suborder == "0":
+                break
+            # Checking if the item chosen is in the menu
+            elif suborder not in self.menu_items:
+                print("This item is not available.")
+            # If it is
             else:
-                print("No orders yet.")
+                while True:
+                    try:
+                        # Asking for the quantity of one item
+                        qty= int(input("How many?: "))
+                        # Checking if it's positive
+                        if qty<=0:
+                            print("Enter a positive number: ")
+                        else:
+                            break
+                    except ValueError:
+                        print("Please enter a valid number.")
+
+                # Adding the item and its quantity to the order
+                order.append((suborder,qty))
+                # Counting the total cost
+                total += qty* self.menu_items[suborder]["price"]
+                
+        print(f"Order taken for {customer_name} at table {table_nbr}.Your total is: {total} DA")
+
+        # Optional rating
+        while True:
+            choice = input("Do you want to rate us?(yes/no): ").strip().capitalize()
+            if choice =="no":
+                rating=0
+                break
+            elif choice=="yes":
+                while True:
+                    rating = float(input("Rate us out of 10: "))
+                    if rating<0 or rating >10:
+                        print("The rating should be from 0 to 10.")
+                    else:
+                        break
+            else:
+                print("Invalid choice. Please answer yes or no.")
+            
+        
+        # Adding all the information to the customer's order
+        self.customer_orders.append({
+            "table":table_nbr,
+            "customer": customer_name,
+            "items":order,
+            "total":total,
+            "rating": rating
+        })
+                    
+        # Incrementing total revenue
+        self.total_revenue+= total
+    
+    # 2- Printing all orders
+    def print_orders(self):
+        
+        # If the list of orders is empty
+        if not self.customer_orders:
+            print("No orders yet.")
+            return
+        
+        print("Customer orders:")
+        for order in self.customer_orders:
+            print(f"\nTable: {order['table']}")
+            print(f"Customer: {order['customer']}")
+            print("Items:")
+        for item, qty in order["items"]:
+            price = self.menu_items[item]["price"]
+            print(f"  - {item} x {qty} = {price * qty} DA")
+            print(f"Total: {order['total']} DA")
+            print(f"Rating: {order['rating']}/10")
+    
+    # 3- Sales report
+    def sales_report(self):
+
+        # If the list of orders is empty
+        if not self.customer_orders:
+            print("No orders yet.")
+            return
+        
+        # New sales report dictionary 
+        sales_report={}
+        # Initializing grand total to 0
+        grand_total=0
+
+        # Looping through customer orders 
+        for order in self.customer_orders:
+            # Looping through items in orders
+            for (item, qty) in order["items"]:
+                price=self.menu_items[item]["price"]
+                # Adding to the revenue
+                revenue=qty*price
+
+                # If the item isn't yet in the sales report
+                if item not in sales_report:
+                    # Adding it and initializing its values to 0
+                    sales_report[item]={"qty":0,"revenue":0}
+
+                # Updating quantity and revenue
+                sales_report[item]["qty"]+= qty
+                sales_report[item]["revenue"]+=revenue
+                # Updating grand total
+                grand_total+=revenue
+                    
+        # Printing sales report
+        print("Sales report")
+        for item,data in sales_report.item():
+            print(f"{item}:{data["qty"]} sold,{data["revenue"]} DA")
+        print(f"Grand total: {grand_total} DA")
+            
+# ------------------------------------------------Main------------------------------------------------
 
 def show_options(menu_options,choice):
     if choice =="0":
@@ -200,7 +321,6 @@ customer_options = {
         int(input("Enter table number: "))
     ),
     "D" : lambda: my_restaurant.customer_order(
-        input("Enter order: ").strip(),
         int(input("Enter table number: "))
     )
 }
